0b62b47127020185e0f3e62285a57a7ef380e737
Keagen <keegenjt@gmail.com>
2023-11-16 15:12:27 -0500

    Added out of bounds checking

--- tetromino.py
+++ tetromino.py
@@ -8,37 +8,37 @@ class TetrominoType(Enum):
     I = [[0, 1, 0, 0],
          [0, 1, 0, 0],
          [0, 1, 0, 0],
-         [0, 1, 0, 0]]
+         [0, 1, 0, 0]];
 
     O = [[0, 0, 0, 0],
          [0, 1, 1, 0],
          [0, 1, 1, 0],
-         [0, 0, 0, 0]]
+         [0, 0, 0, 0]];
 
     L = [[0, 0, 1, 0],
          [1, 1, 1, 0],
          [0, 0, 0, 0],
-         [0, 0, 0, 0]]
+         [0, 0, 0, 0]];
 
     J = [[1, 0, 0, 0],
          [1, 1, 1, 0],
          [0, 0, 0, 0],
-         [0, 0, 0, 0]]
+         [0, 0, 0, 0]];
 
     T = [[0, 1, 0, 0],
          [1, 1, 1, 0],
          [0, 0, 0, 0],
-         [0, 0, 0, 0]]
+         [0, 0, 0, 0]];
 
     S = [[0, 1, 1, 0],
          [1, 1, 0, 0],
          [0, 0, 0, 0],
-         [0, 0, 0, 0]]
+         [0, 0, 0, 0]];
 
     Z = [[1, 1, 0, 0],
          [0, 1, 1, 0],
          [0, 0, 0, 0],
-         [0, 0, 0, 0]]
+         [0, 0, 0, 0]];
 
     '''
     I = pg.image.load(os.path.join('assets', 'Tetrominos.png'));
@@ -53,15 +53,36 @@ class TetrominoType(Enum):
 class Tetromino(pg.sprite.Sprite):
     def __init__(self, tetromino_type: TetrominoType):
         self.type = tetromino_type;
-        self.shape = list(tetromino_type.value); 
+        self.shape = np.copy(tetromino_type.value); 
         self.offset = Vector2(0, 0);
 
     def rotate(self, right=True):
         rot_dir = -1 if right else 1;
-        self.shape = np.rotate90(self.shape, k=rot_dir);
+        self.shape = np.rot90(self.shape, k=rot_dir);
+
+    def shift(self, right=True):
+        left = not right;
+
+        # TODO magic numbers are for default size board [20, 10]
+        x_shift = 1 if right else -1;
+
+        # Check for out of bounds
+        # TODO could break if tetromino shape was empty
+        left_bound = int(self.offset.x);
+        right_bound = int(self.offset.x + 3);
+        if left and left_bound <= 0:
+            overflow_col = abs(left_bound);
+            if not np.all(self.shape[:, overflow_col] == 0):
+                x_shift = 0;
+        elif right and right_bound >= 9:
+            overflow_col = 3 - abs(9 - right_bound);
+            if not np.all(self.shape[:, overflow_col] == 0):
+                x_shift = 0;
+
+        self.offset += Vector2(x_shift, 0);
 
     def fall(self):
-        self.offset += Vector2(1, 0);
+        self.offset += Vector2(0, 1);
 
     def draw(self, screen):
         # TODO Even needed?
