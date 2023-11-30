d02dc966372342198ba13a9de4038688dde77054
Keagen <keegenjt@gmail.com>
2023-11-30 12:52:00 -0500

    Added docstring documentation to Pytris
    Now the Pytris class and each function has some documentation. Also
    corrected a couple local function variables.

--- src/pytris.py
+++ src/pytris.py
@@ -1,10 +1,49 @@
+"""Allows initialization and runtime of a Pytris game. 
+
+Currently only allows creation and then playing. After calling play(),
+game runs until it ends through user input. There are no other
+external controls to stop or pause the game currently.
+"""
 import pygame as pg;
 import numpy as np;
 import random;
 import tetromino;
 
 class Pytris:
+    """Used to play a Pytris game.
+
+    Create with a board size (normal would be (10, 20),
+    and then run with run(). Execution continues until the user
+    presses the window quit button or quit key (ESCAPE).
+
+    Controls:
+        Left/Right Arrow: Move Tetromino left/right
+        Up Arrow: Rotate Tetromino clockwise
+        Down Arrow: Hold to increase fall speed by 3 times
+        Space: Place Tetromino at ghost piece
+        Left Shift: Hold current Tetromino
+        Escape: Quit game
+    """
+
     def __init__(self, board_size: pg.Vector2):
+        """Creates a Pytris game with a specified board size.
+
+        This sets up the variables needed for running the game.
+        play() must be called after initialization to create game
+        window and start main game loop.
+
+        Args:
+            board_size:
+                Vector of board size. x and y are used to set size
+                of board.
+
+        Raises:
+            ValueError: Board was not at least 4x4.
+        """
+
+        if (board_size.x < 4 or board_size.y < 4):
+            raise ValueError("Board size must be at least 4x4");
+
         # Board setup
         self.__width = int(board_size.x);
         self.__height = int(board_size.y);
@@ -43,6 +82,11 @@ class Pytris:
     ####################################
 
     def play(self):
+        """Initializes graphics and input, and runs main game loop.
+
+        Cannot be externally interrupted. Runs main loop until player 
+        presses the window exit button, or the game exit key (ESCAPE).
+        """
         if self.__running:
             return;
 
@@ -70,6 +114,12 @@ class Pytris:
     ####################################
 
     def __play(self):
+        """Main game loop.
+
+        Checks and responds to user input, then executes game logic. 
+        See main Pytris class documentation for inputs.
+        Runs until user presses window exit button, or the escape button.
+        """
         while self.__running:
             # Input reading
             for event in pg.event.get():
@@ -108,7 +158,7 @@ class Pytris:
                     self.__place_tetromino();
 
             # Update display
-            self.__draw_screen();
+            self.__draw_game_board();
             pg.display.update(self.__board_rect);
 
     ####################################
@@ -116,6 +166,16 @@ class Pytris:
     ####################################
 
     def __generate_tetromino(self) -> tetromino.Tetromino:
+        """Instantiates a new Tetromino
+
+        Uses a bucket method to ensure each type of piece is 
+        generated exactly once before being repeated. 
+        Once each type has been used, each one can be used
+        once more, and so on.
+
+        Returns:
+            Tetromino: New random Tetromino instance.
+        """
         if len(self.__bucket) == 0:
             self.__bucket = list(tetromino.TetrominoType);
 
@@ -125,6 +185,15 @@ class Pytris:
         return tetromino.Tetromino(rand_type, self);
 
     def __get_next_tetromino(self) -> tetromino.Tetromino:
+        """Removes and returns next Tetromino in queue
+
+        Fills the queue with random Tetrominos, and returns the 
+        next one in the queue. Returned Tetromino is removed 
+        from queue.
+
+        Returns:
+            Tetromino: Next Tetromino in queue
+        """
         while len(self.__tetromino_queue) <= self.__queue_size:
             self.__tetromino_queue.append(self.__generate_tetromino());
         next_tet = self.__tetromino_queue.pop(0);
@@ -133,6 +202,11 @@ class Pytris:
         return next_tet;
 
     def __place_tetromino(self):
+        """Places falling Tetromino on board.
+
+        Overwites the game board with the current falling Tetromino.
+        Then replaces falling Tetromino with next in queue.
+        """
         tet = self.__falling_tetromino;
         for row_index, row in enumerate(tet.shape):
             for col_index, cell in enumerate(row):
@@ -147,6 +221,13 @@ class Pytris:
         self.__can_hold = True;
 
     def __hold_tetromino(self):
+        """Swaps falling Tetromino with held Tetromino.
+        
+        If another Tetromino is in hold, swaps with that one.
+        If no Tetromino is in hold, puts Tetromino in hold,
+        and sets the falling Tetromino to the next in queue.
+        Can only be used once per block placed.
+        """
         if not self.__can_hold:
             return;
 
@@ -173,7 +254,13 @@ class Pytris:
     # Drawing functions
     ####################################
 
-    def __draw_screen(self):
+    def __draw_game_board(self):
+        """Updates game board graphics.
+        
+        Draws blocks on the graphical game_board. These include
+        the game_board blocks, falling Tetromino blocks, and 
+        falling Tetromino ghost blocks.
+        """
         self.__screen.fill(self.__background_color, self.__board_rect);
 
         # Draw all already placed blocks
@@ -188,22 +275,30 @@ class Pytris:
         self.__draw_tetromino(self.__falling_tetromino);
 
     def __draw_tetromino(self, tetromino):
+        """Draws Tetromino on graphical game board.
+
+        Uses information in passed Tetromino to draw on board. 
+
+        Args:
+            Tetromino:
+                Tetromino to draw on board.
+        """
         for row_index, row in enumerate(tetromino.shape):
             for col_index, block in enumerate(row):
                 if block:
                     board_x = tetromino.offset.x + col_index;
                     board_y = tetromino.offset.y + row_index;
-                    pos = pg.Vector2(board_x, board_y);
                     self.__draw_board_square(tetromino.color,
                                              self.__block_border_color, 
-                                             pos);
+                                             pg.Vector2(board_x, board_y));
 
     def __draw_square(self, color: pg.Color, border_color: pg.Color, 
-                      board_pos: pg.Vector2, size: float):
+                      screen_pos: pg.Vector2, size: float):
+        """Draws a square with a border on screen."""
         border_size = size / 20;
-        outer_rect = pg.Rect(board_pos.x, board_pos.y,
+        outer_rect = pg.Rect(screen_pos.x, screen_pos.y,
                              size, size);
-        inner_rect = pg.Rect(board_pos.x + border_size, board_pos.y + border_size,
+        inner_rect = pg.Rect(screen_pos.x + border_size, screen_pos.y + border_size,
                              size - (2*border_size), size - (2*border_size));
 
         self.__screen.fill(border_color, outer_rect);
@@ -211,12 +306,21 @@ class Pytris:
         
     def __draw_board_square(self, color: pg.Color, border_color: pg.Color, 
                             position: pg.Vector2):
+        """Draws a square on the graphical game board.
+
+        Args:
+            position: Vector of game_board x and y index of block to draw 
+        """
         screen_x = (position.x * self.__block_size) + self.__padding;
         screen_y = (position.y * self.__block_size);
         self.__draw_square(color, border_color, 
                            pg.Vector2(screen_x, screen_y), self.__block_size);
 
     def __draw_left_padding(self):
+        """Draws left UI screen section.
+
+        Draws the held block at the top center.
+        """
         self.__screen.fill(self.__padding_color, self.__left_padding);
 
         if self.__held_tetromino != None:
@@ -240,6 +344,10 @@ class Pytris:
 
 
     def __draw_right_padding(self):
+        """Draws right UI screen section.
+
+        Draws next in queue Tetrominos at the top center going down. 
+        """
         self.__screen.fill(self.__padding_color, self.__right_padding);
 
         for tet_index, tet in enumerate(self.__tetromino_queue):
@@ -265,14 +373,29 @@ class Pytris:
     ####################################
 
     @property
-    def width(self):
+    def width(self) -> int:
+        """Gets width of game_board array.
+        
+        Returns:
+            int: width of game_board array.
+        """
         return self.__width;
 
     @property
-    def height(self):
+    def height(self) -> int:
+        """Gets height of game_board array.
+
+        Returns:
+            int: height of game_board array.
+        """
         return self.__height;
     
     @property
-    def game_board(self):
+    def game_board(self) -> np.ndarray:
+        """Gets a copy of the game_board array.
+
+        Returns:
+            ndarray: copy of game_board numpy array.
+        """
         return np.copy(self.__game_board);
 
